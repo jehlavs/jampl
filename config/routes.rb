1e29eb6b37f1fdadfa1826ddebad58a17ac4812f
hlavsa <hlavsa.jara@seznam.cz>
2018-07-12 21:50:00 +0200

    bg image

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+
+  get 'welcome/index2', to: 'welcome#index2', as: 'index2'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
