e7e71df74c5dfe0efc52c92dd6fd9d25e40c8d50
hlavsa <hlavsa.jara@seznam.cz>
2018-07-14 10:05:39 +0200

    order form, about page, imgs in svg

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
-  get 'welcome/index2', to: 'welcome#index2', as: 'index2'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'welcome/about', to: 'welcome#about', as: 'about'
+  get 'welcome/jobs', to: 'welcome#jobs', as: 'jobs'
+  get 'welcome/customers', to: 'welcome#customers', as: 'customers'
+  
 end
