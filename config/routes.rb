742becf69042bbefdcb4555303c9fd9c92ee4569
hlavsa <hlavsa.jara@seznam.cz>
2018-08-09 16:01:12 +0200

    markos notes pt2

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,9 @@ Rails.application.routes.draw do
   get 'welcome/customers', to: 'welcome#customers', as: 'customers'
   get 'welcome/bim', to: 'welcome#bim', as: 'bim'
   get 'welcome/index2', to: 'welcome#index2', as: 'index2'
-  get 'welcome/index3', to: 'welcome#index3', as: 'index3'
   get 'welcome/facility', to: 'welcome#facility', as: 'facility'
   get 'welcome/contacts', to: 'welcome#contacts', as: 'contacts'
+  get 'welcome/terms-of-use', to: 'welcome#terms-of-use', as: 'terms-of-use'
+
 
 end
