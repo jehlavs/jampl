1f4445b1f3da9480431228ab712bf19389f1333c
hlavsa <hlavsa.jara@seznam.cz>
2018-08-11 14:30:04 +0200

    customers

--- app/assets/javascripts/customers.js
+++ app/assets/javascripts/customers.js
@@ -2,12 +2,11 @@ $('turbolinks:load', function() {
     var selectedClass = "";
     $(".fil-cat").click(function(){ 
     selectedClass = $(this).attr("data-rel"); 
-     $(".section").fadeTo(100, 0.1);
-    $(".section div").not("."+selectedClass).fadeOut().removeClass('scale-anm');
+     $("#portfolio").fadeTo(100, 0.1);
+    $("#portfolio div").not("."+selectedClass).fadeOut().removeClass('scale-anm');
     setTimeout(function() {
       $("."+selectedClass).fadeIn().addClass('scale-anm');
-      $(".section").fadeTo(300, 1);
+      $("#portfolio").fadeTo(300, 1);
     }, 300); 
-    
   });
 });
