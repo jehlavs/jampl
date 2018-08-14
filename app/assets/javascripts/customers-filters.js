eda8cbffdba89093b62149646b2fcfd73bfe976c
hlavsa <hlavsa.jara@seznam.cz>
2018-08-14 09:33:25 +0200

    js and customers btn homepage

---
+++ app/assets/javascripts/customers-filters.js
@@ -0,0 +1,12 @@
+$('turbolinks:load', function() {
+    var selectedClass = "";
+    $(".fil-cat").click(function(){ 
+    selectedClass = $(this).attr("data-rel"); 
+     $("#portfolio").fadeTo(100, 0.1);
+    $("#portfolio div").not("."+selectedClass).fadeOut().removeClass('scale-anm');
+    setTimeout(function() {
+      $("."+selectedClass).fadeIn().addClass('scale-anm');
+      $("#portfolio").fadeTo(300, 1);
+    }, 300); 
+  });
+});
