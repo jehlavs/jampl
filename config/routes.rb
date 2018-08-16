f1133b75258a8171d659677015f6a7fa9249594a
hlavsa <hlavsa.jara@seznam.cz>
2018-08-16 13:12:52 +0200

    marko dumbko bimko

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,13 @@
 Rails.application.routes.draw do
   root 'welcome#index2'
 
-  get 'welcome/about', to: 'welcome#about', as: 'about'
-  get 'welcome/jobs', to: 'welcome#jobs', as: 'jobs'
-  get 'welcome/customers', to: 'welcome#customers', as: 'customers'
-  get 'welcome/bim', to: 'welcome#bim', as: 'bim'
-  get 'welcome/index2', to: 'welcome#index2', as: 'index2'
-  get 'welcome/facility', to: 'welcome#facility', as: 'facility'
-  get 'welcome/contacts', to: 'welcome#contacts', as: 'contacts'
-  get 'welcome/terms-of-use', to: 'welcome#terms-of-use', as: 'terms-of-use'
+  get '/o-nas' => 'welcome#about'
+  get '/reference' => 'welcome#customers'
+  get '/bim' => 'welcome#bim'
+  get '/prehled-cinnosti' => 'welcome#facility'
+  get '/kontakt' => 'welcome#contacts'
+  get '/zasady-pouziti' => 'welcome#terms-of-use'
+  get '/kariera' => 'welcome#jobs'
 
-
+  
 end
