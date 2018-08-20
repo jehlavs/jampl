165b32f99b66cb0fbf65a9247bbf27217a7d4896
Ondřej Ezr <ezrondre@fit.cvut.cz>
2018-08-20 20:51:59 +0200

    filters working

--- app/assets/javascripts/customers-filters.js
+++ app/assets/javascripts/customers-filters.js
@@ -1,17 +1,17 @@
 $('turbolinks:load', function() {
     var selectedClass = "";
 
-    $('.fil-cat').click(function() {  
+    $('.fil-cat').click(function() {
       $(this).addClass('active');
     });
 
-    $(".fil-cat").click(function(){ 
-      selectedClass = $(this).attr("data-rel"); 
+    $(".fil-cat").click(function(){
+      selectedClass = $(this).attr("data-rel");
       $("#portfolio").fadeTo(100, 0.1);
-      $("#portfolio div").not("."+selectedClass).fadeOut().removeClass('scale-anm');
+      $("#portfolio > div.tile").not("."+selectedClass).fadeOut().removeClass('scale-anm');
       setTimeout(function() {
         $("."+selectedClass).fadeIn().addClass('scale-anm');
         $("#portfolio").fadeTo(300, 1);
-      }, 300); 
+      }, 300);
   });
 });
